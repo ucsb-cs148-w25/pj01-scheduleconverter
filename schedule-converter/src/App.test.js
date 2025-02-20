4532896da0db9de680b97e22f1be6670e2389b20
yungongWang <yungong@ucsb.edu>
2025-02-20 11:20:07 +0800

    yungong - update for integration test

--- schedule-converter/src/App.test.js
+++ schedule-converter/src/App.test.js
@@ -18,39 +18,41 @@ beforeAll(() => {
 });
 
 // unit test for render
-test('test for renders - heading', () => {
-  render(<App />);
-  const heading = screen.getByRole("heading", { name: /Schedule to Google Calendar/i });
-  expect(heading).toBeInTheDocument();
-});
+describe('Unit Tests', () => {
+  test('test for renders - heading', () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { name: /Schedule to Google Calendar/i });
+    expect(heading).toBeInTheDocument();
+  });
 
-test('test for renders - button', () => {
-  render(<App />);
-  const sign_in_button = screen.getByRole("button", { name: /Sign in with Google/i });
-  const sign_out_button = screen.getByRole("button", { name: /Sign out/i });
-  const test_button = screen.getByRole("button", { name: /Test GCAL/i });
-  const search_button = screen.getByRole("button", { name: /Search/i });
-  const convert_button = screen.getByRole("button", { name: /Convert Schedule to Google Calendar/i });
-  expect(sign_in_button).toBeInTheDocument();
-  expect(sign_out_button).toBeInTheDocument();
-  expect(test_button).toBeInTheDocument();
-  expect(search_button).toBeInTheDocument();
-  expect(convert_button).toBeInTheDocument();
-});
+  test('test for renders - button', () => {
+    render(<App />);
+    const sign_in_button = screen.getByRole("button", { name: /Sign in with Google/i });
+    const sign_out_button = screen.getByRole("button", { name: /Sign out/i });
+    const test_button = screen.getByRole("button", { name: /Test GCAL/i });
+    const search_button = screen.getByRole("button", { name: /Search/i });
+    const convert_button = screen.getByRole("button", { name: /Convert Schedule to Google Calendar/i });
+    expect(sign_in_button).toBeInTheDocument();
+    expect(sign_out_button).toBeInTheDocument();
+    expect(test_button).toBeInTheDocument();
+    expect(search_button).toBeInTheDocument();
+    expect(convert_button).toBeInTheDocument();
+  });
 
-test('test for renders - text', () => {
-  render(<App />);
-  const text1 = screen.getByText(/Enter Perm Number:/i);
-  expect(text1).toBeInTheDocument();
-});
+  test('test for renders - text', () => {
+    render(<App />);
+    const text1 = screen.getByText(/Enter Perm Number:/i);
+    expect(text1).toBeInTheDocument();
+  });
 
-test('test for render - placeholders', () => {
-  render(<App />);
-  const perm_input = screen.getByPlaceholderText('Perm Number');
-  const quarter_input = screen.getByPlaceholderText('Quarter (YYYYQ)');
-  expect(perm_input).toBeInTheDocument();
-  expect(quarter_input).toBeInTheDocument();
-});
+  test('test for render - placeholders', () => {
+    render(<App />);
+    const perm_input = screen.getByPlaceholderText('Perm Number');
+    const quarter_input = screen.getByPlaceholderText('Quarter (YYYYQ)');
+    expect(perm_input).toBeInTheDocument();
+    expect(quarter_input).toBeInTheDocument();
+  });
+})
 
 // integration test
 jest.mock('react-google-calendar-api');
